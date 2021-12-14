dfb6016931053774c74e7a04c02e24d842fda97d
TechWithAnirudhApp <96112172+TechWithAnirudhApp@users.noreply.github.com>
2021-12-14 22:35:39 +0530

    Update auth-callback.js

--- functions/auth-callback.js
+++ functions/auth-callback.js
@@ -1,10 +1,6 @@
-const querystring = require('querystring')
-const { config, oauth } = require('./utils/auth')
-const { getUser } = require('./utils/netlify-api')
-const VALID_URLS = [
-    'anikode.techwithanirudh.tech',
-    'grapesjs-netlify.techwithanirudh.repl.co',
-]
+const querystring = require("querystring");
+const { config, oauth } = require("./utils/auth");
+const { getUser } = require("./utils/netlify-api");
 
 /* Function to handle netlify auth callback */
 exports.handler = async (event, context) => {
@@ -13,15 +9,15 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 401,
       body: JSON.stringify({
-        error: 'Not authorized',
-      })
-    }
+        error: "Not authorized",
+      }),
+    };
   }
 
   /* Grant the grant code */
-  const code = event.queryStringParameters.code
+  const code = event.queryStringParameters.code;
   /* state helps mitigate CSRF attacks & Restore the previous state of your app */
-  const state = querystring.parse(event.queryStringParameters.state)
+  const state = querystring.parse(event.queryStringParameters.state);
 
   try {
     /* Take the grant code and exchange for an accessToken */
@@ -29,14 +25,14 @@ exports.handler = async (event, context) => {
       code: code,
       redirect_uri: config.redirect_uri,
       client_id: config.clientId,
-      client_secret: config.clientSecret
-    })
+      client_secret: config.clientSecret,
+    });
 
-    const authResult = oauth.accessToken.create(authorizationToken)
+    const authResult = oauth.accessToken.create(authorizationToken);
 
-    const token = authResult.token.access_token
+    const token = authResult.token.access_token;
 
-    const user = await getUser(token)
+    const user = await getUser(token);
 
     // return {
     //   statusCode: 200,
@@ -51,43 +47,30 @@ exports.handler = async (event, context) => {
     const encodedUserData = querystring.stringify({
       email: user.email || "NA",
       full_name: user.full_name || "NA",
-      avatar: user.avatar_url || "NA"
-    })
+      avatar: user.avatar_url || "NA",
+    });
 
-    /* Check if the url contains any of the valid urls by matching the regex */
-    const isValidUrl = VALID_URLS.some(url => state.url.match(url))
-    
-    /* If the url is not valid, return an error */
-    if (!isValidUrl) {
-        return {
-            statusCode: 401,
-            body: JSON.stringify({
-                error: 'Not authorized',
-            })
-        }
-    }
-
-    const URI = `${state.url}#${encodedUserData}&csrf=${state.csrf}&token=${Buffer.from(token, 'binary').toString('base64')}`
-    console.log('URI', URI)
+    const URI = `${state.url}#${encodedUserData}&csrf=${
+      state.csrf
+    }&token=${Buffer.from(token, "binary").toString("base64")}`;
+    console.log("URI", URI);
     /* Redirect user to authorizationURI */
     return {
       statusCode: 302,
       headers: {
         Location: URI,
-        'Cache-Control': 'no-cache' // Disable caching of this response
+        "Cache-Control": "no-cache", // Disable caching of this response
       },
-      body: '' // return body for local dev
-    }
-
-
+      body: "", // return body for local dev
+    };
   } catch (e) {
-    console.log('Access Token Error', e.message)
-    console.log(e)
+    console.log("Access Token Error", e.message);
+    console.log(e);
     return {
       statusCode: e.statusCode || 500,
       body: JSON.stringify({
         error: e.message,
-      })
-    }
+      }),
+    };
   }
-}
+};
